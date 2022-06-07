35af0bfe438a6709c9042a55b33f75415545fc0e
William <80454082+the-little-way@users.noreply.github.com>
2022-06-07 23:27:37 +0000

    Update searchBar.js

--- src/searchBar.js
+++ src/searchBar.js
@@ -1,8 +1,19 @@
+import {useState} from 'react';
+
 function SearchBar() {
+
+  //let data = null;
+
+  let handleSubmit(){
+    fetch()
+    .then()
+    .then()
+  };
+
   return (
     <div className="search-bar">
-      <form action="/search" method="post">
-        <input className="city-input" placeholder="City Name"/>
+      <form onSubmit = {handleSubmit}>
+        <input id="cityInput" className="city-input" placeholder="City Name" required />
         <button className="submit-btn" type="submit">Search</button>
       </form>
     </div>
